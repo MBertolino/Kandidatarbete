ecb961b03faab1e0e9095e54f0ca4c6e07a6f515
mbertolino <mattias.bertolino@gmail.com>
2016-04-13 15:18:42 +0200

    Our own crossval for choosing lambda

--- GeneralizedRegress.m
+++ GeneralizedRegress.m
@@ -6,7 +6,7 @@ xTrain = xTrain(:,pred);
 XTrain = [ones(size(xTrain(:,1))) xTrain];
 
 % Implement Ridge, don't penalize the intercept
-lambda = 1e-3;
+lambda = 1e3;
 ridgeEye = eye(nPred+1);
 ridgeEye(1,1) = 0;
 XRidge = [XTrain; (lambda*ridgeEye)];
@@ -14,7 +14,7 @@ yRidge = [yTrain; zeros(nPred+1,1)];
 
 
 % Solve the system using quadprog
-H = XRidge'*XRidge;
+H = XRidge'*XRidge;% + ridgeEye*lambda;
 f = -XRidge'*yRidge;
 % lb = zeros(length(pred)+1,1);          % Possible lower bound
 % ub = Inf*ones(length(pred)+1,1);       % Possible upper bound
