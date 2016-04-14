98b74e2763dba6894d174d7c4f0bd32ff9ab905d
mbertolino <mattias.bertolino@gmail.com>
2016-04-14 16:13:14 +0200

    Fixar kovariater

--- RidgeRegress.m
+++ RidgeRegress.m
@@ -3,13 +3,13 @@ function [b, yHat] = RidgeRegress(yTrain, XTrain)
 [row, col] = size(XTrain);
 
 % Penalty factor
-lambda = 1e-3;
+lambda = 1e5;
 ridgeEye = eye(col);
 ridgeEye(1,1) = 0;
 
 % Ridge data
-XRidge = [XTrain; (lambda*ridgeEye)];
-yRidge = [yTrain; zeros(col,1)];
+% XRidge = [XTrain; (lambda*ridgeEye)];
+% yRidge = [yTrain; zeros(col,1)];
 
 % Solving with lsqr
 b = lsqr(XRidge,yRidge);
