30593efe55f3fc61af3db8869a6abce729bd4eb7
mbertolino <mattias.bertolino@gmail.com>
2016-04-16 15:08:49 +0200

    Saknas trading

--- MLR.m
+++ MLR.m
@@ -4,7 +4,7 @@ clear;
 load('KexJobbData.mat')
 depMarket = 1;                 % Dependent Market
 indepMarket = 1:40;               % Possible independent markets
-predTime = 1;                  % How many days to predict
+predTime = 51;                  % How many days to predict
 lag = 1;                       % How many days ago we look at the indep markets
 
 
@@ -24,25 +24,27 @@ trend = zeros(size(dates));
 ret = trend;
 
 % Training data
-%for i = 1+lag(end)+predTime:length(dates)-predTime
-for i = 1+lag(end)+predTime:1+lag(end)+predTime+500
+for i = 1+lag(end)+predTime:length(dates)-predTime
+%for i = 1+lag(end)+predTime:1+lag(end)+predTime+19
     yTrain(i-lag(end)-predTime,:) = clPr(i,depMarket) - clPr(i-predTime,depMarket);
     xTrain(i-lag(end)-predTime,:) = clPr(i-predTime,indepMarket) - clPr(i-lag-predTime,indepMarket);
 end
 
-% Validation data
+% Validation dates
 yVal = yTrain(1+predTime:end,:);
 xVal = xTrain(1+predTime:end,:);
 
 % Standardize data and add intercept
 [xTrain, mu, sigma] = zscore(xTrain);
 XTrain = [ones(size(xTrain(:,1))) xTrain];
-xVal = (xVal - mu')./sigma';        
+[nr,nc] = size(xVal);
+xVal = (xVal - repmat(mu,length(yVal),1));
+xVal = xVal./(sigma'*ones(1,nr))';
 XVal = [ones(size(xVal(:,1))) xVal];
 
 % Normal regress
 method{1} = 'Regress';
-[b1, yHat1] = NormalRegress(yTrain, XTrain);
+[b1, yHat] = NormalRegress(yTrain, XTrain);
 
 % % Lsq Ridge
 % method{2} = 'Identity Ridge';
@@ -53,23 +55,26 @@ method{1} = 'Regress';
 yPred = XVal*b1;
 % yPred(:,2) = XVal*b2;
 
-% % Calculate profit
-% gamma = zeros(size(trend));
-% gamma(trend > 0) = 1;
-% gamma(trend < 0) = -1;
-% ret = yVal.*gamma;
-% profit = cumsum(ret);
+% Calculate profit
+trend = sign(yPred);
+gamma = zeros(size(trend));
+gamma(trend > 0) = 1;
+gamma(trend < 0) = -1;
+ret = yVal.*gamma;
+profit = cumsum(ret);
+
 
 % % Make it same length as dates
 % yTrain = [yTrain; NaN*ones(predTime,1)];
 % yHat = [yHat; NaN*ones(predTime,1)];
+
 
 %% Plots
 figure()
 for ip = 1:1
     %p(ip) = subplot(2,1,ip);
     hold on;
-    plot(dates, [yTrain yHat(:,ip)])
+    plot(dates(2+lag(end)+predTime:end), [yTrain yHat(:,ip)])
     ylabel('$$$');
     xlabel('Time [Days]');
     title(['Multiple Linear Regression Fit, ' method(ip)]);
