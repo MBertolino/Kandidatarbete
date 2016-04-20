70da44b367f45440dc6b315279cfa1ed8cd44c4f
mbertolino <mattias.bertolino@gmail.com>
2016-04-20 11:22:50 +0200

    multiple markets

--- MLR.m
+++ MLR.m
@@ -2,18 +2,16 @@ clear;
 
 % Load Data
 load('KexJobbData.mat')
-depMarket = 1;                   % Dependent Market
-indepMarket = 1:40;              % Possible independent markets
-lag = 1:4;                       % How many days ago we look at the indep markets
-
-% Use this time period
 [dates, clPr] = removeNaN(dates, closingPrice);
+
+% Parameters
+depMarket = 38;
+indepMarket = 1:40;
+lag = 1:4;                       % How many days ago we look at the indep markets
 
 trainTime = 400;
 predTime = 21;                   % How many days to predict
-tradePeriods = 150;
-
-
+tradePeriods = 160;
 
 
 %% Regression
@@ -27,60 +25,56 @@ yPred2 = yVal;
 datez = yVal;
 b = zeros(lag(end)*length(indepMarket)+1,2);
 
-% Sliding window
-for j = 1:tradePeriods
-    for i = 1:trainTime-predTime-lag(end)
-        yTrain(i,:) = clPr(i+lag(end)+(j+1)*predTime,depMarket) - clPr(i+lag(end)+j*predTime,depMarket);
-        xTemp = repmat(clPr(i+lag(end)+j*predTime,indepMarket),lag(end),1) - clPr(i+lag(end)-lag+j*predTime,indepMarket);
-        xTrain(i,:) = reshape(xTemp.',1,[]);
+for m = 1:length(depMarket)
+    
+    % Sliding window
+    for j = 1:tradePeriods
+        for i = 1:trainTime-predTime-lag(end)
+            yTrain(i,:) = clPr(i+lag(end)+(j+1)*predTime,depMarket(m)) - clPr(i+lag(end)+j*predTime,depMarket(m));
+            xTemp = repmat(clPr(i+lag(end)+j*predTime,indepMarket),lag(end),1) - clPr(i+lag(end)-lag+j*predTime,indepMarket);
+            xTrain(i,:) = reshape(xTemp.',1,[]);
+        end
+          
+        % Standardize data and add intercept
+        [xTrain, mux, sigmax] = zscore(xTrain);
+        XTrain = [ones(size(xTrain(:,1))) xTrain];
+        [yTrain, muy, sigmay] = zscore(yTrain);
+        
+        % OLS Regression
+        method{1} = 'OLS Regression';
+        b(:,1) = regress(yTrain,XTrain);
+        
+        % Ridge Regression
+        method{2} = 'Ridge Regression';
+        b(:,2) = RidgeRegress(yTrain, XTrain);
+        
+        
+        %% Prediction & Validation
+        % Prediction
+        xTemp = repmat(clPr(i+lag(end)+(j+1)*predTime,indepMarket),lag(end),1) - clPr(i+lag(end)-lag+(j+1)*predTime,indepMarket);
+        xVal = reshape(xTemp.',1,[]);
+        xVal = (xVal - mux)./sigmax;
+        XVal = [ones(size(xVal(:,1))) xVal];
+        
+        yPred1(j) = XVal*b(:,1);             % Regress
+        yPred2(j) = XVal*b(:,2);             % Ridge
+        
+        % Validation
+        yVal(j) = clPr(i+lag(end)+(j+2)*predTime,depMarket(m)) - clPr(i+lag(end)+(j+1)*predTime,depMarket(m));
+        yVal(j) = (yVal(j) - muy)/sigmay;
+        
+        % Dates adjustment
+        datez(j) = dates(i+lag(end)+(j+1)*predTime);
     end
     
-    
-    % Standardize data and add intercept
-    [xTrain, mux, sigmax] = zscore(xTrain);
-    XTrain = [ones(size(xTrain(:,1))) xTrain];
-    [yTrain2, muy, sigmay] = zscore(yTrain);
-    
-    
-    % Normal regress
-    method{1} = 'Regress';
-    b(:,1) = regress(yTrain,XTrain);
-    
-    % Lsq Ridge
-    method{2} = 'Identity Ridge';
-    b(:,2) = RidgeRegress(yTrain, XTrain);
-    
-    
-    %% Prediction
-    
-    % Prediction
-    xTemp = repmat(clPr(i+lag(end)+(j+1)*predTime,indepMarket),lag(end),1) - clPr(i+lag(end)-lag+(j+1)*predTime,indepMarket);
-    xVal = reshape(xTemp.',1,[]);
-    xVal = xVal - repmat(mux,length(xVal(j)),1);
-    xVal = xVal./sigmax;
-    XVal = [ones(size(xVal(:,1))) xVal];
-    
-    yPred1(j,1) = XVal*b(:,1); % Regress
-    yPred1(j,2) = yPred1(j,1) + sigmay; % + 1 std
-    yPred1(j,3) = yPred1(j,1) - sigmay; % - 1 std
-    yPred2(j) = XVal*b(:,2); % Ridge
-    
-    
-    % Validation
-    yVal(j) = clPr(i+lag(end)+(j+2)*predTime,depMarket) - clPr(i+lag(end)+(j+1)*predTime,depMarket);
-    
-    
-    % Dates adjustment
-    datez(j) = dates(i+lag(end)+(j+1)*predTime);
+    % Calculate profit
+    yPred = yPred2;
+    gamma = sign(yPred);
+    ret = yVal.*gamma;
+    profit(:,m) = cumsum(ret);
 end
 
-% Calculate profit
-yPred = yPred1;
-gamma = sign(yPred);
-for k = 1:3
-    ret(:,k) = yVal.*gamma(:,k);
-    profit(:,k) = cumsum(ret(:,k));
-end
+profit = sum(profit,2);
 
 %% Plots
 
@@ -93,8 +87,8 @@ datetick('x')
 
 figure()
 hold on;
-plot(datez,yPred)
+plot(datez,yVal)
 hold on;
-% plot(datez,yVal)
+plot(datez,yPred)
 legend('Real data','Prediction', 'Location', 'Northeast');
 datetick('x')
