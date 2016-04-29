7109d502640037a536c854f81577c5992a4d5bee
mbertolino <mattias.bertolino@gmail.com>
2016-04-29 13:38:04 +0200

    Added investment

--- MLR.m
+++ MLR.m
@@ -2,20 +2,20 @@ clear;
 tic;
 
 % Assets
-depAsset = 36;
+depAsset = 36:37;
 indepAsset = 36:40;
 Ld = length(depAsset);
 Li = length(indepAsset);
 
 % Prediction Param
 lag = 1:20;                       % How many days ago we look at the indep markets
-predTime = 21;                   % How many days to predict
+predTime = 21;                    % How many days to predict
 trainTime = 300;
 lambda = 2e2;
 
 % Investment
 bankStart = 10000;
-risk = 1;
+risk = 0.05;
 
 % Load Data
 load('KexJobbData.mat')
@@ -33,11 +33,11 @@ xTrain = zeros(trainTime - lag(end) - predTime, lag(end)*Li);
 yVal = zeros(tradePeriods, Ld);
 yPred = zeros(tradePeriods, 2*Ld);
 sigmay = yVal;
-datez = yVal;
-bank = yVal;
-bank(1) = bankStart;
 b = zeros(lag(end)*Li + 1, 2*Ld);
 profitTot = zeros(tradePeriods, 2);
+bank = zeros(tradePeriods, 2*Ld);
+datez = bank;
+bank(1,:) = bankStart;
 
 
 %% Regression
@@ -60,12 +60,13 @@ for j = 1:tradePeriods
         xTemp = repmat(clPr(i + lag(end) + j*predTime, indepAsset),lag(end),1) ...
             - clPr(i + lag(end) - lag + j*predTime, indepAsset);
         xTrain(i,:) = reshape(xTemp.', 1, []);
+        
     end
     
     % Standardize data and add intercept
+    [yTrain, muy, sigmay] = zscore(yTrain);
     [xTrain, mux, sigmax] = zscore(xTrain);
     XTrain = [ones(size(xTrain(:,1))) xTrain];
-    [yTrain, muy, sigmay] = zscore(yTrain);
     
     for m = 1:Ld
         % OLS Regression
@@ -85,65 +86,64 @@ for j = 1:tradePeriods
     xVal = reshape(xTemp.', 1, []);
     xVal = (xVal - mux)./sigmax;
     XVal = [ones(size(xVal(:,1))) xVal];
-    
     yPred(j,:) = XVal*b;
     
     % Validation
-    yVal(j,:) = clPr(i+lag(end) + (j+2)*predTime, depAsset) ...
+    yVal(j,:) = clPr(i + lag(end) + (j+2)*predTime, depAsset) ...
         - clPr(i + lag(end) + (j+1)*predTime, depAsset);
     yVal(j,:) = (yVal(j,:) - muy)./sigmay;
     
     % Dates adjustment
-    datez(j) = dates(i + lag(end) + (j+1)*predTime);
+    datez(j,:) = dates(i + lag(end) + (j+1)*predTime);
 end
 
-% Strategy
+
+%% Strategy
 for i = 1:2
     gamma(:,1+(i-1)*Ld:i*Ld) = sign(yPred(:,1+(i-1)*Ld:i*Ld));
-    ret(:,1+(i-1)*Ld:i*Ld) = yVal.*gamma(:,1+(i-1)*Ld:i*Ld)/std(yVal);
+    ret(:,1+(i-1)*Ld:i*Ld) = bsxfun (@rdivide, yVal.*gamma(:,1+(i-1)*Ld:i*Ld), std(yVal));
     profit(:,1+(i-1)*Ld:i*Ld) = cumsum(ret(:,1+(i-1)*Ld:i*Ld));
-    profitTot(:,i) = sum(profit(:,1+(i-1)*Ld:i*Ld),2);
+%     profitTot(:,i) = sum(profit(:,1+(i-1)*Ld:i*Ld),2);
     infoRet(i) = mean(ret(:,1+(i-1)*Ld:i*Ld))/std(ret(:,1+(i-1)*Ld:i*Ld)) ...
         * sqrt(250); % Annualized
 end
 
 for ii = 2:length(ret)
-    bank(ii) = bank(ii-1)*(1 + risk*ret(ii-1,1));
+    bank(ii,:) = bank(ii-1,:).*(1 + risk*ret(ii-1,:));
 end
 
 
 %% Plots
-
 % Plot accumulated profit for each market
-figure()
-hold on;
-plot(datez(:,1), profit)
-title('Accumulated Profit')
-ylabel('Risk-adjusted profit')
-xlabel('Time [years]')
-legend('OLS', 'Ridge')
-datetick('x')
-
-% Plot accumulated total profit
-figure()
-plot(datez(:,1), profitTot)
-title('Accumulated Total Profit')
-ylabel('Risk-adjusted profit')
-xlabel('Time [years]')
-legend('OLS', 'Ridge')
-datetick('x')
-
-%Plot expected returns for each market
-figure()
-hold on;
-plot(datez(:,1),yVal)
-hold on;
-plot(datez(:,1),yPred)
-title('Expected return')
-ylabel('Risk-adjusted return')
-xlabel('Time [years]')
-legend('Real data','Prediction', 'Location', 'Northeast');
-datetick('x')
+% figure()
+% hold on;
+% plot(datez(:,1), profit)
+% title('Accumulated Profit')
+% ylabel('Risk-adjusted profit')
+% xlabel('Time [years]')
+% legend('OLS', 'Ridge')
+% datetick('x')
+%
+% % Plot accumulated total profit
+% figure()
+% plot(datez(:,1), profitTot)
+% title('Accumulated Total Profit')
+% ylabel('Risk-adjusted profit')
+% xlabel('Time [years]')
+% legend('OLS', 'Ridge')
+% datetick('x')
+%
+% %Plot expected returns for each market
+% figure()
+% hold on;
+% plot(datez(:,1),yVal)
+% hold on;
+% plot(datez(:,1),yPred)
+% title('Expected return')
+% ylabel('Risk-adjusted return')
+% xlabel('Time [years]')
+% legend('Real data','Prediction', 'Location', 'Northeast');
+% datetick('x')
 
 % Plot the investment
 figure()
