73d1e1dda7305df9f6f01e67bcf7b440647c4caf
mbertolino <mattias.bertolino@gmail.com>
2016-05-12 16:12:15 +0200

    Ready to publish results

--- MLR.m
+++ MLR.m
@@ -15,21 +15,11 @@ tic;
 % Load Data
 load('KexJobbData.mat')
 
-% Assets
-class = 4;
-name = {'Equities', 'Fixed Income'};
-% depAsset = find(assetClass == class); % 1:13 14:22 23:35 36:40
-% indepAsset = depAsset;
-depAsset = [1; 13]';
-indepAsset = [1; 13]';
-Ld = length(depAsset(:,1));
-Li = length(indepAsset(:,1));
-
 % Prediction Param
-lag = [1 2];
-predTime = 1;                    % How many days to predict
-trainTime = 100;
-lambda = [0];
+lag = [10 20];
+predTime = 21;                    % How many days to predict
+trainTime = 600;
+lambda = [0 1e0 1e1 1e2 1e3];
 Ll = length(lambda);
 
 % Investment Param
@@ -38,127 +28,161 @@ risk = 0.05;
 
 % Remove NaN's
 % Start at 02-Jan-2009
-% End at 28-Jan-2016 7447
+% End at 28-Jan-2016
 [dates, clPr] = removeNaN(dates(7447 - trainTime - 2*predTime + 1:end), ...
     closingPrice(7447 - trainTime - 2*predTime + 1:end, :));
 tradePeriods = floor((length(dates) - trainTime - predTime)/predTime) - 1;
 diffClPr = diff(clPr);
 
-for l = 1:length(lag)
-    % Pre-allocating
-    b = zeros(lag(l)*Li, 1);
-    yTrain = zeros(trainTime - lag(l) - predTime, Ld);
-    xTrain = zeros(trainTime - lag(l) - predTime, lag(l)*Li);
-    yVal = zeros(tradePeriods, Ld);
-    yPred = zeros(tradePeriods, Ll*Ld);
-    sigmay = zeros(1, Ld);
-    holding = zeros(tradePeriods, Ll*Ld);
-    holdingTot = zeros(tradePeriods, Ll);
-    datez = holdingTot;
-    holding(1,:) = bankStart;
-    gamma = zeros(tradePeriods, Ll*Ld);
-    
-    %% Regression
-    % Data relating assets to be predicted, y, and assets to use as
-    % predictors, x. For each day in a tradePeriods sized window y and x are
-    % related.
-    % After a prediction the training window is moved so only the latest data
-    % points are used as training data as they are assumed to be more accurate
-    
-    h = waitbar(0,'Total time...');
-    for iDep = 1:length(depAsset(1,:))
-        % Sliding window
-        for j = 1:tradePeriods
-            %     yTrain(1:end - predTime, :) = yTrain(predTime + 1:end, :);
-            %     xTrain(1:end - predTime, :) = xTrain(predTime + 1:end, :);
-            for i = 1 + lag(l):trainTime - predTime
-                yTrain(i-lag(l), :) = clPr(i + j*predTime, depAsset(:,iDep)) ...
-                    - clPr(i + (j-1)*predTime, depAsset(:,iDep));
-                xTemp = diffClPr(i - lag(l) + (j-1)*predTime : ...
-                    i - 1 + (j-1)*predTime, indepAsset(:,iDep))';
-                xTrain(i-lag(l), :) = reshape(xTemp.', 1, []);
-            end
-            
-            % Standardize data and add intercept
-            [yTrain, muy, sigmay] = zscore(yTrain);
-            [xTrain, mux, sigmax] = zscore(xTrain);
-            
-            % For every invested asset, calculate the regression coefficients
-            % using both OLS and Ridge
-            b(:,1:Ll*Ld) = RidgeRegress(yTrain, xTrain, lambda);
-            
-            
-            %% Prediction & Validation
-            % Prediction of the change  in price of each asset
-            % XVal - are the predictors
-            % yPred - is the predicted change in price of each asset
-            xTemp = diffClPr(i - lag(l) + j*predTime : ...
-                i - 1 + j*predTime, indepAsset(:,iDep))';
-            xVal = reshape(xTemp, 1, []);
-            xVal = (xVal - mux)./sigmax;
-            yPred(j,:) = xVal*b;
-            
-            % Smart positioning (optional)
-            if j > 99
-                gamma(j,:) = yPred(j,:)./mean(abs(yPred(j-99:j,:)));
-            else
-                gamma(j,:) = yPred(j,:)./mean(abs(yPred(1:j,:)));
-            end
-            
-            % Validation
-            % yVal - is the actual price change measured at the end of the
-            % prediction time
-            yVal(j,:) = clPr(i + (j+1)*predTime, depAsset(:,iDep)) ...
-                - clPr(i + j*predTime, depAsset(:,iDep));
-            yVal(j,:) = (yVal(j,:) - muy)./sigmay;
-            
-            % Dates adjustment
-            % At each predicted day, the date is extracted
-            datez(j,:) = dates(i + (j+1)*predTime);
-            
-            waitbar(j/tradePeriods)
-        end
-        
-        
-        %% Strategy
-        % gamma - is the position to take for each asset
-        % ret - is the risk adjusted return for taking a position
-        % sharpe - is the information quotient for a strategy
-        
-        % Position and return
-        gamma(abs(gamma) > 1) = sign(gamma(abs(gamma) > 1)); % Smart
-        %     gamma = sign(yPred);                           % +/- 1
-        ret = bsxfun (@rdivide, repmat(yVal,1,Ll).*gamma, repmat(std(yVal),1,Ll));
-        
-        % Calculate the evolution of a holding for each asset
-        for ih = 2:length(ret)
-            holding(ih,:) = holding(ih - 1, :).*(1 + risk*ret(ih - 1, :));
-        end
-        
-        % Calculate the evolution of the total holding
-        for il = 1:Ll
-            sharpe(il,iDep) = mean(ret(:, 1 + (il-1)*Ld:il*Ld)) ...
-                /std(ret(:, 1 + (il-1)*Ld:il*Ld)) ...
-                * sqrt(250); % Annualized
-            holdingTot(:,il) = holdingTot(:,il) + sum(holding(:, (il-1)*Ld + 1:il*Ld), 2)/Ld;
-        end
-        
-        
-        %% Plots
-        % Plot the evolution of the total holding
-        figure()
-        plot(datez, holdingTot)
-        ylabel('Holding [$]') % ;)
-        xlabel('Time [Days]')
-        title(['Holding using MLR on ' name(iDep) ' with lag ' lag(l)])
-        str = cellstr(num2str(lambda', 'lambda = %d'));
-        legend(str, 'Location', 'NorthWest');
-        datetick('x')
-        
+% Assets
+name = {'Equities 1', 'Equities 2', 'Fixed Income 1', 'Fixed Income 2', ...
+    'Commodities 1', 'Commodities 2', 'Foreign Exchange'};
+assetIndex = 1:2;
+% Calculate all asset classes at ones
+for asset = assetIndex
+    switch(asset)
+        case(1)
+            depAsset = [1:6]';
+            indepAsset = [1:6]';
+        case(2)
+            depAsset = [7:13]';
+            indepAsset = [7:13]';
+        case(3)
+            depAsset = [14:18]';
+            indepAsset = [14:18]';
+        case(4)
+            depAsset = [19:22]';
+            indepAsset = [19:22]';
+        case(5)
+            depAsset = [23:29]';
+            indepAsset = [23:29]';
+        case(6)
+            depAsset = [30:35]';
+            indepAsset = [30:35]';
+        case(7)
+            depAsset = [36:40]';
+            indepAsset = [36:40]';
     end
-    holdingTot = holdingTot/iDep;
-    disp(['Sharpe ratio for lag ' sharpe])
-    close(h);
+    Ld = length(depAsset(:,1));
+    Li = length(indepAsset(:,1));
+    
+    for l = 1:length(lag)
+        % Pre-allocating
+        b = zeros(lag(l)*Li, 1);
+        yTrain = zeros(trainTime - lag(l) - predTime, Ld);
+        xTrain = zeros(trainTime - lag(l) - predTime, lag(l)*Li);
+        yVal = zeros(tradePeriods, Ld);
+        yPred = zeros(tradePeriods, Ll*Ld);
+        sigmay = zeros(1, Ld);
+        holding = zeros(tradePeriods, Ll*Ld);
+        holdingTot = zeros(tradePeriods, Ll);
+        datez = holdingTot;
+        holding(1,:) = bankStart;
+        gamma = zeros(tradePeriods, Ll*Ld);
+        
+        %% Regression
+        % Data relating assets to be predicted, y, and assets to use as
+        % predictors, x. For each day in a tradePeriods sized window y and x are
+        % related.
+        % After a prediction the training window is moved so only the latest data
+        % points are used as training data as they are assumed to be more accurate
+        
+        h = waitbar(0,['Lag: ' num2str(l) '/' num2str(length(lag)) ...
+            ', Asset class: ' num2str(asset) '/' num2str(assetIndex(end))]);
+        for iDep = 1:length(depAsset(1,:))
+            % Sliding window
+            for j = 1:tradePeriods
+                %     yTrain(1:end - predTime, :) = yTrain(predTime + 1:end, :);
+                %     xTrain(1:end - predTime, :) = xTrain(predTime + 1:end, :);
+                for i = 1 + lag(l):trainTime - predTime
+                    yTrain(i-lag(l), :) = clPr(i + 1 + j*predTime, depAsset(:,iDep)) ...
+                        - clPr(i + 1 + (j-1)*predTime, depAsset(:,iDep));
+                    xTemp = diffClPr(i - lag(l) + (j-1)*predTime : ...
+                        i - 1 + (j-1)*predTime, indepAsset(:,iDep))';
+                    xTrain(i-lag(l), :) = reshape(xTemp.', 1, []);
+                end
+                
+                % Standardize data and add intercept
+                [yTrain, muy, sigmay] = zscore(yTrain);
+                [xTrain, mux, sigmax] = zscore(xTrain);
+                
+                % For every invested asset, calculate the regression coefficients
+                % using both OLS and Ridge
+                b(:,1:Ll*Ld) = RidgeRegress(yTrain, xTrain, lambda);
+                
+                
+                %% Prediction & Validation
+                % Prediction of the change  in price of each asset
+                % XVal - are the predictors
+                % yPred - is the predicted change in price of each asset
+                xTemp = diffClPr(i - lag(l) + j*predTime : ...
+                    i - 1 + j*predTime, indepAsset(:,iDep))';
+                xVal = reshape(xTemp, 1, []);
+                xVal = (xVal - mux)./sigmax;
+                yPred(j,:) = xVal*b;
+                
+                % Smart positioning (optional)
+                if j > 99
+                    gamma(j,:) = yPred(j,:)./mean(abs(yPred(j-99:j,:)));
+                else
+                    gamma(j,:) = yPred(j,:)./mean(abs(yPred(1:j,:)));
+                end
+                
+                % Validation
+                % yVal - is the actual price change measured at the end of the
+                % prediction time
+                yVal(j,:) = clPr(i + (j+1)*predTime, depAsset(:,iDep)) ...
+                    - clPr(i + j*predTime, depAsset(:,iDep));
+                yVal(j,:) = (yVal(j,:) - muy)./sigmay;
+                
+                % Dates adjustment
+                % At each predicted day, the date is extracted
+                datez(j,:) = dates(i + (j+1)*predTime);
+                
+                waitbar(j/tradePeriods)
+            end
+            
+            
+            %% Strategy
+            % gamma - is the position to take for each asset
+            % ret - is the risk adjusted return for taking a position
+            % sharpe - is the information quotient for a strategy
+            
+            % Position and return
+            gamma(abs(gamma) > 1) = sign(gamma(abs(gamma) > 1)); % Smart
+            %     gamma = sign(yPred);                           % +/- 1
+            ret = bsxfun (@rdivide, repmat(yVal,1,Ll).*gamma, repmat(std(yVal),1,Ll));
+            
+            % Calculate the evolution of a holding for each asset
+            for ih = 2:length(ret)
+                holding(ih,:) = holding(ih - 1, :).*(1 + risk*ret(ih - 1, :));
+            end
+            
+            % Calculate the evolution of the total holding
+            for il = 1:Ll
+                sharpe(il,iDep) = mean(ret(:, 1 + (il-1)*Ld:il*Ld)) ...
+                    /std(ret(:, 1 + (il-1)*Ld:il*Ld)) ...
+                    * sqrt(250/predTime); % Annualized
+                holdingTot(:,il) = holdingTot(:,il) + sum(holding(:, (il-1)*Ld + 1:il*Ld), 2)/Ld;
+            end
+            
+            
+            %% Plots
+            % Plot the evolution of the total holding
+            figure()
+            plot(datez, holdingTot)
+            ylabel('Holding [$]') % ;)
+            xlabel('Time [Days]')
+            title(['Holding using MLR on ' name(asset) ' with lag ' lag(l)])
+            str = cellstr(num2str(lambda', 'lambda = %d'));
+            legend(str, 'Location', 'NorthWest');
+            datetick('x')
+            
+        end
+        holdingTot = holdingTot/iDep;
+        disp(['Sharpe ratio for lag ' num2str(lag(l)) ', and asset ' num2str(asset) ': ' num2str(sharpe')])
+        close(h);
+    end
 end
 holdingTot = holdingTot/iDep;
 hold off;
